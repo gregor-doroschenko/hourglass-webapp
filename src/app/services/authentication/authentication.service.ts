09740e2d96b28c3a57540a2f8cad3bfbb030d4b7
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-09-24 22:57:46 +0200

    create authentication service

---
+++ src/app/services/authentication/authentication.service.ts
@@ -0,0 +1,115 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { defaults } from '../../defaults';
+
+const currentUserAuthTokenKey: string = 'currentUserAuthToken';
+const redmineApiUrlKey: string = 'redmineApiUrl';
+const expirationDateKey: string = 'expirationDate';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private usersUrl: string = '/users/current.json';
+
+  redirectUrl: string;
+
+  constructor(private http: HttpClient) { }
+
+  private setExpirationDate(rememberMe: boolean = false) {
+    const currentDate = new Date();
+    let newExpirationDate = currentDate.setHours(currentDate.getHours() + 4);
+    if (rememberMe) {
+      newExpirationDate = currentDate.setDate(currentDate.getDate() + 7);
+    }
+    const expirationDate = localStorage.getItem(expirationDateKey);
+    if (expirationDate) {
+      localStorage.removeItem(expirationDateKey);
+    }
+    localStorage.setItem(expirationDateKey, JSON.stringify(newExpirationDate));
+  }
+
+  private getExpirationDate(): number {
+    const expirationDate = localStorage.getItem(expirationDateKey);
+    if (expirationDate) {
+      return JSON.parse(expirationDateKey);
+    }
+    return null;
+  }
+
+  isExpirationDateValid(): boolean {
+    const currentDate = Date.now();
+    const expirationDate = this.getExpirationDate();
+    if (currentDate >= expirationDate) {
+      this.clearLocalStorage();
+      return false;
+    }
+    return true;
+  }
+
+  private setRedmineApiUrl(redmineUrl: string) {
+    const redmineApiUrl = localStorage.getItem(redmineApiUrlKey);
+    if (redmineApiUrl) {
+      localStorage.removeItem(redmineApiUrlKey);
+    }
+    localStorage.setItem(redmineApiUrlKey, this.prepareRedmineUrl(redmineUrl));
+  }
+
+  private getRedmineApiUrl(): string {
+    const redmineUrl = localStorage.getItem(redmineApiUrlKey);
+    if (redmineUrl) {
+      return redmineUrl;
+    }
+    return null;
+  }
+
+  private prepareRedmineUrl(url: string): string {
+    const lastChar = url.slice(-1);
+    if (lastChar === '/') {
+      return url.slice(0, url.length - 1);
+    }
+    return url;
+  }
+
+  checkIfRedmineUrlExist(): boolean {
+    return !!this.getRedmineApiUrl();
+  }
+
+  private setAuthToken(token: string) {
+    const tokenString = localStorage.getItem(currentUserAuthTokenKey);
+    if (tokenString) {
+      localStorage.removeItem(currentUserAuthTokenKey);
+    }
+    localStorage.setItem(currentUserAuthTokenKey, token);
+  }
+
+  getAuthToken(): string {
+    const tokenString = localStorage.getItem(currentUserAuthTokenKey);
+    if (tokenString) {
+      return tokenString;
+    }
+    return null;
+  }
+
+  login(redmineUrl: string, apiKey: string): Observable<any> {
+    this.setRedmineApiUrl(redmineUrl);
+    this.setAuthToken(apiKey);
+    const url = this.getRedmineApiUrl() + this.usersUrl;
+    let headers: HttpHeaders = defaults.getDefaultHeaders();
+    if (this.getRedmineApiUrl() && this.getAuthToken()) {
+      headers = headers.append('X-Redmine-API-Key', this.getAuthToken());
+    }
+
+    return this.http.get<any>(url, { headers: headers });
+  }
+
+  logout() {
+    this.clearLocalStorage();
+  }
+
+  private clearLocalStorage() {
+    localStorage.clear();
+  }
+}
