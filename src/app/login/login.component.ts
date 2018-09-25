4cf815f8bf7663613f548a4e52d2de948d0c28c0
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-09-26 00:12:20 +0200

    save username to localstorage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication/authentication.service';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,8 @@ export class LoginComponent implements OnInit {
   isLoggedIn: boolean;
 
   constructor(private authenticationService: AuthenticationService,
-              private router: Router) {
+              private router: Router,
+              private userService: UserService) {
     this.isLoggedIn = this.authenticationService.isExpirationDateValid();
   }
 
@@ -25,6 +27,7 @@ export class LoginComponent implements OnInit {
   login() {
     if (this.redmineUrl && this.apiKey) {
       this.authenticationService.login(this.redmineUrl, this.apiKey, this.rememberMe).subscribe(result => {
+        this.userService.setUserName(result.user.firstname, result.user.lastname);
         const redirect = this.authenticationService.redirectUrl ? this.authenticationService.redirectUrl : '/';
         this.router.navigate(['/']);
       }, error => {
