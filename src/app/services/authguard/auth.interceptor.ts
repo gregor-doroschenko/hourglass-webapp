c670c3157e106f6bc3b6a7082c1804479fc45e8a
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-09-27 00:13:59 +0200

    improve auth interceptor

--- src/app/services/authguard/auth.interceptor.ts
+++ src/app/services/authguard/auth.interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs';
+import { defaults } from '../../defaults';
 import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
@@ -12,8 +13,12 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authHeader = this.injector.get(AuthenticationService).getAuthToken();
+    let reqHeaders = req.headers;
+    reqHeaders = reqHeaders.append('Content-Type', 'application/json');
+    reqHeaders = reqHeaders.append('Accept', 'application/json');
+    reqHeaders = reqHeaders.append('X-Redmine-API-Key', authHeader);
 
-    const authReq = req.clone({ headers: req.headers.set('X-Redmine-API-Key', authHeader)});
+    const authReq = req.clone({ headers: reqHeaders });
 
     return next.handle(authReq);
   }
