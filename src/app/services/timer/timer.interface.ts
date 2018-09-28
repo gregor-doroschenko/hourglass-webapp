acd3d3dbf13cdf15a1d36e795736882640861ff7
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-09-28 23:28:52 +0200

    create interface for time tracker object

---
+++ src/app/services/timer/timer.interface.ts
@@ -0,0 +1,15 @@
+export interface TimeTrackerObject {
+  time_tracker: Partial<TimeTracker>;
+}
+
+export interface TimeTracker {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  user_id: number;
+  start: string;
+  project_id: number;
+  activity_id: number;
+  issue_id: number;
+  comments: string;
+}
