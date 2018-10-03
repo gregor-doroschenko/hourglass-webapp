7520e80e2f0723eef887f64f0b1685ac90e52ab0
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-10-03 21:16:37 +0200

    group time logs by date

--- src/app/components/timer/time-logs/time-logs.component.ts
+++ src/app/components/timer/time-logs/time-logs.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material';
-import { TimeTracker } from '../../../services/timer/timer.interface';
+import { GroupedTimeLogs, TimeTracker } from '../../../services/timer/timer.interface';
 import { TimelogDeleteDialogComponent } from './timelog-delete-dialog/timelog-delete-dialog.component';
 
 @Component({
@@ -13,7 +13,8 @@ export class TimeLogsComponent implements OnInit {
   @Input() timeLogs: TimeTracker[];
 
   @Output() deleteTimeLogEvent: EventEmitter<number> = new EventEmitter<number>();
-  groupedTimeLogs: any;
+
+  groupedTimeLogs: GroupedTimeLogs[] = [];
 
   constructor(private dialog: MatDialog) { }
 
@@ -22,6 +23,41 @@ export class TimeLogsComponent implements OnInit {
     this.timeLogs.map(timeLog => {
       timeLog.diff_time = this.getTimeDifference(timeLog.start, timeLog.stop);
       return timeLog;
+    });
+    this.groupTimeLogsByDate();
+  }
+
+  getDaysForLastTwoWeeks(): number[] {
+    const now = Date.now();
+    const currentDate = new Date(now).getDate();
+    let twoWeeksCount = 14;
+    const allDays: number[] = [];
+    while (twoWeeksCount >= 0) {
+      const date = new Date(now).setDate(-twoWeeksCount + currentDate);
+      allDays.push(new Date(date).getTime());
+      twoWeeksCount--;
+    }
+    return allDays.sort((a, b) => b - a);
+  }
+
+  groupTimeLogsByDate() {
+    const lastTwoWeeksDays = this.getDaysForLastTwoWeeks();
+    lastTwoWeeksDays.forEach(day => {
+      const group: GroupedTimeLogs = {
+        int_date: day,
+        time_logs: []
+      };
+      const timeLogs = this.timeLogs.filter(timelog => {
+        const timelogDate = new Date(timelog.created_at).setHours(0, 0, 0, 0);
+        const dayDate = new Date(day).setHours(0, 0, 0, 0);
+        if (new Date(timelogDate).getTime() === new Date(dayDate).getTime()) {
+          return timelog;
+        }
+      });
+      if (timeLogs.length !== 0) {
+        group.time_logs = timeLogs;
+      }
+      this.groupedTimeLogs.push(group);
     });
   }
 
@@ -58,6 +94,7 @@ export class TimeLogsComponent implements OnInit {
     const timelogIndex = this.timeLogs.findIndex(timelog => timelog.id === timeLogId);
     if (timelogIndex !== -1) {
       this.timeLogs.splice(timelogIndex, 1);
+      this.groupTimeLogsByDate();
     }
   }
 
