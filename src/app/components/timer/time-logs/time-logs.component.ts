7c526f0e84fa24ff57f7a2d98f8105ca1f29e3e6
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-10-03 20:01:59 +0200

    fix sorting function

--- src/app/components/timer/time-logs/time-logs.component.ts
+++ src/app/components/timer/time-logs/time-logs.component.ts
@@ -27,7 +27,7 @@ export class TimeLogsComponent implements OnInit {
 
   sortByDate() {
     this.timeLogs.sort((a, b) => {
-      return new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime();
+      return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
     });
   }
 
